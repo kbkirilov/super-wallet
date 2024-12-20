5bf1353007c4ebff764c497c6ab6cb877b612e71
Owner <kirilovmail90@gmail.com>
2024-12-20 22:45:41 +0200

    feat(db): create initial schema and create.sql and insert.sql scripts

---
+++ db/create.sql
@@ -0,0 +1,64 @@
+create table currencies
+(
+    currency_id int auto_increment primary key ,
+    currency_code char(3) not null unique ,
+    currency_name varchar(30) not null unique ,
+    country varchar(30) not null
+);
+
+create table users
+(
+    user_id int auto_increment primary key ,
+    username varchar(20) not null unique ,
+    password varchar(15) not null ,
+    first_name varchar(20) not null ,
+    last_name varchar(20) not null ,
+    date_of_birth datetime not null ,
+    address varchar(200) not null ,
+    is_admin boolean
+);
+
+create table pocket_money
+(
+    pocket_money_id int auto_increment primary key ,
+    amount double not null ,
+    currency_id int ,
+    user_id int ,
+
+    constraint fk_pocket_money_currency_id
+        foreign key (currency_id) references currencies (currency_id) ,
+    constraint fk_pocket_money_user_id
+        foreign key (user_id) references users (user_id)
+);
+
+create table statuses
+(
+    status_id int auto_increment primary key ,
+    status_name varchar(20) unique
+);
+
+create table wallets
+(
+    wallet_id int auto_increment primary key ,
+    name varchar(40) ,
+    currency_id int ,
+    user_id int ,
+    status_id int default 1,
+    balance double default 0 ,
+    created_at datetime default current_timestamp ,
+
+    constraint fk_wallets_currency_id
+        foreign key (currency_id) references currencies (currency_id) ,
+    constraint fk_wallets_user_id
+        foreign key (user_id) references users (user_id) ,
+    constraint fk_wallets_status_id
+        foreign key (status_id) references statuses (status_id)
+);
+
+create table admins
+(
+    user_id int auto_increment primary key ,
+
+    constraint fk_admins_user_id
+        foreign key (user_id) references users (user_id)
+);
---
+++ db/insert.sql
@@ -0,0 +1,45 @@
+insert into currencies (currency_id, currency_code, currency_name, country)
+values
+    (1, 'BGN', 'Bulgarian Lev', 'Bulgaria'),
+    (2, 'USD', 'United States Dollar', 'United States'),
+    (3, 'EUR', 'Euro', 'European Union'),
+    (4, 'GBP', 'British Pound Sterling', 'United Kingdom'),
+    (5, 'JPY', 'Japanese Yen', 'Japan');
+
+insert into users (user_id, username, password, first_name, last_name, date_of_birth, address, is_admin)
+values
+    (1, 'kitodartodorov', 'password', 'Kitodar', 'Todorov',
+     '1978-01-12 00:00:00', 'ul. Pirotska 45, ap. 12, Sofia 1303, Bulgaria', false),
+    (2, 'markrober', 'password', 'Mark', 'Rober',
+     '2000-04-01 00:00:00', '1234 Elm Street, Apt 56B, Springfield, IL 62701, USA', false),
+    (3, 'jeremyclarkson', 'password', 'Jeremy', 'Clarkson',
+     '1964-11-07 00:00:00', '123 Baker Street, London, W1U 6RP, United Kingdom', false),
+    (4, 'petergriffin', 'password', 'Peter', 'Grifin',
+     '1999-01-31 00:00:00', '31 Spooner Street', true);
+
+
+insert into pocket_money (pocket_money_id, amount, currency_id, user_id)
+values
+    (1, 10000.00, 1, 1),
+    (2, 5000.00, 3, 1),
+    (3, 250.00, 2, 2),
+    (4, 50.00, 4, 3);
+
+insert into statuses (status_id, status_name)
+values
+    (1, 'Active'),
+    (2, 'Frozen'),
+    (3, 'Suspended');
+
+insert into wallets (wallet_id, name, currency_id, user_id)
+values
+    (1, 'Main', 1, 1),
+    (2, 'Savings', 3, 1),
+    (3, 'Main', 2, 2),
+    (4, 'Dream home', 4, 3);
+
+insert into admins (user_id)
+values
+    (4);
+
+
