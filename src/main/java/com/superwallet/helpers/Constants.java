8cfd1a6fcd06608d636275f924df5d137b86dd74
Owner <kirilovmail90@gmail.com>
2025-01-02 23:34:55 +0200

    refactor: constant name change and cleanup

--- src/main/java/com/superwallet/helpers/Constants.java
+++ src/main/java/com/superwallet/helpers/Constants.java
@@ -10,12 +10,10 @@ public class Constants {
     public static final String YOU_ARE_ALLOWS_TO_USE_ONLY_YOUR_POCKET_MONEY = "You are not allowed to perform action with other users pocket money.";
     public static final String YOU_DON_T_HAVE_ENOUGH_FUNDS_ERROR_MESSAGE = "You don't have enough funds to do the transaction.";
     public static final String THE_CURRENCIES_DOES_NOT_MATCH = "The currencies does not match.";
-    public static final String FROZEN_STATUS_WALLET_ERROR_MESSAGE = "You can't send/receive funds because the wallet is frozen.";
     public static final String NEGATIVE_TRANSACTION_SUM_ERROR_MESSAGE = "The transaction sum cannot be negative.";
     public static final String LESS_TRANSACTION_SUM_ERROR_MESSAGE = "The transaction sum must be greater than %s";
 
     public static final String FROZEN_STATUS = "Frozen";
-    public static final String ACTIVE_STATUS = "Active";
 
     public static final BigDecimal MIN_TRANSACTION_SUM = new BigDecimal("0.01");
 
